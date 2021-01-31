fa9341d040651639205a13494b7bb135899b77ef
MCTzOCK <dev@mctzock.de>
2021-01-31 14:54:06 +0100

    Added Spawn Teleport on Join

--- src/main/java/net/craftions/lobby/events/EventPlayerJoin.java
+++ src/main/java/net/craftions/lobby/events/EventPlayerJoin.java
@@ -4,6 +4,7 @@
 package net.craftions.lobby.events;
 
 import net.craftions.lobby.config.Config;
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -13,5 +14,6 @@ public class EventPlayerJoin implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent e){
         e.setJoinMessage(((String) Config.getInstance("message").get("join")).replaceAll("%player", e.getPlayer().getName()).replaceAll("&", "§"));
+        e.getPlayer().teleport((Location) Config.getInstance("locations").get("spawn"));
     }
 }
